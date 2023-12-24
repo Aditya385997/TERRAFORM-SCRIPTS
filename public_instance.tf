91e947129b9dce1419b2e0185595fe1f43091a11
Aditya385997 <adityagole123@gmail.com>
2023-12-25 04:06:47 +0530

    change the code ami successfully accessible to the public bastion server or the ec2 machine

--- public_instance.tf
+++ public_instance.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "bastion-server" {
-  ami           = "ami-079db87dc4c10ac91" 
+  ami           = "ami-05d47d29a4c2d19e1" 
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public-subnet-1.id
   security_group = [aws_security_group.bastion_server_sg.id]
