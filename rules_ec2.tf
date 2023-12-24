7c8fb6d6113a365a648685ea545ee2c8027d1e63
Aditya385997 <adityagole123@gmail.com>
2023-12-25 05:07:27 +0530

    changes security group rule from ssh to tcp

--- rules_ec2.tf
+++ rules_ec2.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "bastion_server_sg" {
   ingress {
     from_port = 22
     to_port   = 22
-    protocol  = "ssh"
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
