3a6c9f070a2ae7de6b6f88c0cbb1c10c766acc83
Aditya385997 <adityagole123@gmail.com>
2023-12-25 05:03:33 +0530

    error solved security_groups variable name

--- public_instance.tf
+++ public_instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion-server" {
   ami           = "ami-0c7217cdde317cfec" 
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public-subnet-1.id
-  security_group = [aws_security_group.bastion_server_sg.id]
+  security_groups = [aws_security_group.bastion_server_sg.id]
   key_name = "slave"
 
   tags = {
