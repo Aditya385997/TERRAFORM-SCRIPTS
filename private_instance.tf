f11ad32bc49a6de9b4b5857ff714db6793f7e835
Aditya385997 <adityagole123@gmail.com>
2023-12-26 01:54:49 +0530

    removed rules file and add security group to the instance tf file

---
+++ private_instance.tf
@@ -0,0 +1,30 @@
+
+resource "aws_security_group" "app_server_sg" {
+  name        = "app-server-security-group"
+  description = "Security group for the app server"
+
+  vpc_id = aws_vpc.this.id
+
+  // Add inbound rules as needed
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+
+resource "aws_instance" "app_servers" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.app_server_sg.id]
+  subnet_id =  aws_subnet.private-subnet-1.id
+  key_name = "slave"
+
+  tags = {
+    Name = "APP-SERVER"
+  }
+}
--- public_instance.tf
+++ public_instance.tf
@@ -1,3 +1,18 @@
+resource "aws_security_group" "bastion_server_sg" {
+  name        = "bastion-server-sg"
+  description = "Security group for the bastion server"
+
+  vpc_id = aws_vpc.this.id
+
+  // Add inbound rules as needed
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 resource "aws_instance" "bastion-server" {
   ami           = "ami-0c7217cdde317cfec" 
