642ede94f4490f2b362305a18b971d1767cf55b8
Aditya385997 <adityagole123@gmail.com>
2023-12-25 12:22:46 +0530

    create a change so that a whenever a ec2 instance is launch public ip is automaticaly allocated to the ec2 machine

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "public-subnet-1" {
   vpc_id     = aws_vpc.this.id
   cidr_block = "10.100.1.0/24"
   availability_zone = "us-east-1a"
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-1"
   }
@@ -22,7 +22,7 @@ resource "aws_subnet" "public-subnet-2" {
   vpc_id     = aws_vpc.this.id
   cidr_block = "10.100.2.0/24"
   availability_zone = "us-east-1b"
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-2"
   }
