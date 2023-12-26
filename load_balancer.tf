4244dc8c97d2a2de57e22967bef4c4fdb2930c04
Aditya385997 <adityagole123@gmail.com>
2023-12-27 02:42:29 +0530

    load balancer created successfully but few bugs to solve on ec2 machine

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,9 +1,18 @@
-resource "aws_lb_target_group" "nginx" {
-  name     = "NGINX-APPLICATION"
+
+
+resource "aws_lb_target_group" "app_server_target_group" {
+  name     = "APP-SERVER"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.this.id
-  
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "app_server_attachment" {
+  target_group_arn = aws_lb_target_group.app_server_target_group.arn
+  target_id        = aws_instance.app_servers.id
 }
 
 resource "aws_security_group" "load_balancer_sg" {
@@ -21,41 +30,24 @@ resource "aws_security_group" "load_balancer_sg" {
   }
 }
 
-resource "aws_lb" "Loadbalancer" {
-  name               = "LoadBalancer"
+
+resource "aws_lb" "load_balancer" {
+  name               = "upgrad-load-balancer"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.load_balancer_sg.id]
-  subnets = [aws_subnet.public-subnet-1.id,aws_subnet.public-subnet-2]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+  enable_deletion_protection = false
+  
 }
 
-
-
-resource "aws_lb_listener" "load_balancer_listener" {
-  load_balancer_arn = aws_lb.Loadbalancer.arn
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.load_balancer.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type             = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      status_code  = "200"
-      message_body = "OK"
-    }
-  }
-}
-
-
-resource "aws_lb_listener_rule" "load_balancer_listener_rule" {
-  listener_arn = aws_lb_listener.load_balancer_listener.arn
-
-  action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.nginx.arn
-  }
-
-  condition {
-    
+    target_group_arn = aws_lb_target_group.app_server_target_group.arn
   }
 }
--- private_instance.tf
+++ private_instance.tf
@@ -13,6 +13,7 @@ resource "aws_security_group" "app_server_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress  {
+    from_port = 80
     to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
