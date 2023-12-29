dc6cb201c0fcca8a41536e330d227237ab0bf08c
Aditya385997 <adityagole123@gmail.com>
2023-12-29 16:39:42 +0530

    created a outbound rule for the loadbalancer in security group

--- load_balancer.tf
+++ load_balancer.tf
@@ -28,6 +28,12 @@ resource "aws_security_group" "load_balancer_sg" {
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
