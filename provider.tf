a2769db77e4ef4ee1db2e83c74ba7a97aaa99a3e
Aditya385997 <adityagole123@gmail.com>
2024-01-01 15:06:08 +0530

    comment is modified in provider.tf file

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
     bucket = "upgrad-12213" //s3 bucket name
     key    = "states/terraform.tfstate"//s3 bucket object to store the tfstate are change globally
     region = "us-east-1"
-    dynamodb_table = "terraform"//dynamodb name
+    dynamodb_table = "terraform"//dynamodb name LockID is compulsory so that during working on collabration if one user is working so the tfstate will be locked for other user 
   }
 
 }
